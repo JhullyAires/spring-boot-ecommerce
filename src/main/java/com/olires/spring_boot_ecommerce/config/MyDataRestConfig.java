473173eb51e7b9cd15aa4dccaebad8474de53d9d
Jhully Aires <jhullyolires@gmail.com>
2025-10-12 12:48:16 -0300

    bug fix noticed when committing detail view

--- src/main/java/com/olires/spring_boot_ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/olires/spring_boot_ecommerce/config/MyDataRestConfig.java
@@ -7,7 +7,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.core.mapping.HttpMethods;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -28,20 +27,20 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
         entityManager = theEntityManager;
     }
 
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) { // CorsRegistry cors
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) { // CorsRegistry cors
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 
         // disable HTTP methods for Product: PUT, POST and DELETE
         config.getExposureConfiguration()
-            .forDomainType(Product.class)
-            .withItemExposure((metdata, HttpMethods) -> HttpMethods.disable(theUnsupportedActions))
-            .withCollectionExposure((metdata, HttpMethods) -> HttpMethods.disable(theUnsupportedActions));
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
         // disable HTTP methods for ProductCategory: PUT, POST and DELETE
         config.getExposureConfiguration()
-            .forDomainType(ProductCategory.class)
-            .withItemExposure((metdata, HttpMethods) -> HttpMethods.disable(theUnsupportedActions))
-            .withCollectionExposure((metdata, HttpMethods) -> HttpMethods.disable(theUnsupportedActions));
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     
         // call an internal helper method
         exposeIds(config);
